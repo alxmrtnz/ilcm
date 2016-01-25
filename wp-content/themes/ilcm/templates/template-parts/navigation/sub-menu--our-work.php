b50bf3412c11dfb198a1ef440353f0b0a5dac72c
Alex Martinez <amart1892@gmail.com>
2016-01-25 00:00:21 -0500

    updates to sub menus

--- wp-content/themes/ilcm/templates/template-parts/navigation/sub-menu--our-work.php
+++ wp-content/themes/ilcm/templates/template-parts/navigation/sub-menu--our-work.php
@@ -1,14 +1,19 @@
 <div class="nav-bar__sub-menu sub-menu--work" data-name="work">
 	<div class="row">
-		<div class="medium-8 medium-offset-4 columns">
+		<div class="medium-3 medium-offset-1 columns">
+			<div class="sub-menu__featured-img-left">
+				<img src="http://www.lorempixel.com/200/150" alt="" class="img-bordered-small">
+			</div>
+		</div>
+		<div class="medium-8 columns">
 			Resources
 
-			<ul class="resources-sub-menu__immigration-resources">
+			<ul class="sub-nav__ul">
 				<?php
 
 	            $menu = array(
 	                'theme_location'  => '',
-	                'menu'            => 'Immigration Resources Menu',
+	                'menu'            => 'Our Work Menu',
 	                'container'       => false,
 	                'container_class' => '',
 	                'container_id'    => '',
@@ -29,32 +34,7 @@
 
 	         ?>
 			</ul>	
-			<ul class="resources-sub-menu__immigration-resources">
-				<?php
-
-	            $menu = array(
-	                'theme_location'  => '',
-	                'menu'            => 'Immigration Resources Menu',
-	                'container'       => false,
-	                'container_class' => '',
-	                'container_id'    => '',
-	                'menu_class'      => 'mainMenuList',
-	                'menu_id'         => 'nav',
-	                'echo'            => true,
-	                'fallback_cb'     => 'wp_page_menu',
-	                'before'          => '',
-	                'after'           => '',
-	                'link_before'     => '',
-	                'link_after'      => '',
-	                'items_wrap'      => '%3$s',
-	                'depth'           => 0,
-	                'walker'          => ''
-	            );
-
-	            wp_nav_menu($menu);
-
-	         ?>
-			</ul>		
+					
 
 		</div>
 	</div>
