3e02813f839958f88a33bea11384fc7f8218a8bf
Alex Martinez <amart1892@gmail.com>
2016-01-30 20:35:54 -0500

    updates to homepage

--- wp-content/themes/ilcm/front-page.php
+++ wp-content/themes/ilcm/front-page.php
@@ -59,34 +59,66 @@ get_header(); ?>
 			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 
 		<?php endif; ?>
+	</div> <!-- end .slider.slider--homepage -->
+</section> <!-- end .hero -->
+
+
+<section class="intro">
+	<div class="row">
+		<div class="small-12 large-8 columns">
+			<div class="intro-header-block">
+				<!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/stock/stock1.jpg" alt=""> -->
+				<h3 class="heading--medium intro-header-block__heading">Supporting the pursuit<br>of the American Dream</h3>
+			</div>
+			<p>The Immigrant Law Center of Minnesota (ILCM) was founded in 1976 as Oficina Legal, a program of Southern Minnesota Regional Legal Services (SMRLS).</p>
+			<p>The agency was later renamed to reflect the increasing diversity of the immigrant population it served. Over the last decade ILCM has established itself as Minnesota’s premier provider of comprehensive immigration legal services to low-income clients of all nationalities.</p>
+		</div>
+		<div class="small-12 large-4 columns">
+			<div class="intake-block">
+				<div class="intake-block__header">
+					<h4>Client Intake</h4>
+				</div>
+				<div class="intake-block__details">
+					<p>Intake Hours</p>
+					<p>Mon, Tues, Wed</p>
+					<p>from 12 pm - 1:30 pm</p>
+					<p>Thursdays</p>
+					<p>6 pm - 7:30 pm</p>
+					<p>
+						Please call our client line at
+	1-800-223-1368 during intake hours to enter the appointment lottery system to speak to a legal staff member.
+					</p>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section> <!-- end .intro -->
+
+<section class="what-we-do">
+	<div class="row">
+		<div class="small-12 columns">
+			<h2>What We Do</h2>
+		</div>
+	</div>
+</section>
+
+<section class="our-projects">
+	<div class="row">
+		<div class="small-12 columns">
+			<h2>Our Project</h2>
+		</div>
+	</div>
+</section>
+
+<section class="donate">
+	<div class="row">
+		<div class="small-12 columns">
+			<h3>Support ILCM</h3>
+		</div>
 	</div>
 </section>
 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-		<p>hellooooasdfihaosdifhaoisdhfosih</p>
-		<div class="row">
-			<div class="small-12 columns">
-				this is a column test
-			</div>
-		</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
