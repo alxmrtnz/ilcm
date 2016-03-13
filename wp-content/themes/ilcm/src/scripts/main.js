a841f2d9827d7bb5f3614c6b04e2975055f5e70f
Alex Martinez <amart1892@gmail.com>
2016-03-13 12:14:31 -0400

    started playing with adding cookie functionality to show escape route modal (in main.js). Need to finish later

--- wp-content/themes/ilcm/src/scripts/main.js
+++ wp-content/themes/ilcm/src/scripts/main.js
@@ -2,14 +2,52 @@
 var $ = require('jquery');
 
 // Application Imports
+var safe = require('./safe-browsing.js');
 var hero = require('./hero.js');
 var Header = require('./header.js');
+
 
 
 // Here is where we'll launch our functionality.
 (function($) {
     
     hero();
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = 'expires='+d.toUTCString();
+        document.cookie = cname + '=' + cvalue + '; ' + expires;
+    }
+
+    function getCookie(cname) {
+        var name = cname + '=';
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return '';
+    }
+
+    function checkCookie(cname) {
+        var user = getCookie(cname);
+        if (user != '') {
+            alert('Welcome again ' + user);
+        } else {
+            user = 'true';
+            if (user != '' && user != null) {
+                setCookie('username', user, 365);
+            }
+        }
+    }
+
+    function popUp() {
+    	// alert('Welcome again ');
+    	checkCookie('username');
+    }
+    // popUp();
+    
     
     var header = new Header();
 })($);
---
+++ wp-content/themes/ilcm/src/scripts/safe-browsing.js
@@ -0,0 +1,7 @@
+"use strict";
+/*jshint strict: true */
+
+module.exports = function () {
+
+	
+};
