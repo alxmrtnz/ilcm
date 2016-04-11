bc0229cf19936f94bd0ab2a28cd588fa68256162
Alex Martinez <amart1892@gmail.com>
2016-04-11 19:10:25 -0400

    hide 'Posts' from wordpress admin added to functions.php

--- wp-content/themes/ilcm/functions.php
+++ wp-content/themes/ilcm/functions.php
@@ -65,6 +65,40 @@ Misc Theme Functions
 *****************************************/
 
 
+function post_remove ()      //creating functions post_remove for removing menu item
+{ 
+   remove_menu_page('edit.php');
+}
+
+add_action('admin_menu', 'post_remove');   //adding action for triggering function call
+
+function hide_add_new() {
+global $submenu;
+    // For Removing New Posts from Admin Menu
+    unset($submenu['post-new.php?post_type=post'][10]);
+    // For Removing New Pages
+    unset($submenu['post-new.php?post_type=page'][10]);
+   // For Removing CPTs
+    unset($submenu['post-new.php?post_type=custom_post_type'][10]);
+}
+add_action('admin_menu', 'hide_add_new');
+
+//Thanks to Howdy_McGee
+function remove_admin_bar_links() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('new-post');
+    $wp_admin_bar->remove_menu('new-page');
+    $wp_admin_bar->remove_menu('new-cpt');
+}
+add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
+
+
+function disable_new_post() {
+if ( get_current_screen()->post_type == 'my_post_type' )
+    wp_die( "You ain't allowed to do that!" );
+}
+add_action( 'load-post-new.php', 'disable_new_post' );
+
 
 /**
  * Define custom post type capabilities for use with Members
