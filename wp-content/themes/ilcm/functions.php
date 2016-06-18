45a42112b44d21c62515a73b58a4c3b918f77d6b
Alex Martinez <amart1892@gmail.com>
2016-06-18 09:32:07 -0400

    added 'Appearance > Editor'

--- wp-content/themes/ilcm/functions.php
+++ wp-content/themes/ilcm/functions.php
@@ -67,8 +67,10 @@ Misc Theme Functions
 
 function post_remove ()      //creating functions post_remove for removing menu item
 {
-   remove_menu_page('edit.php');
+   // remove_menu_page('edit.php');
 }
+
+define('DISALLOW_FILE_EDIT', false);
 
 add_action('admin_menu', 'post_remove');   //adding action for triggering function call
 
